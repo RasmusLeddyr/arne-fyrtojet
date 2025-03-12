e3239d9fd09293bcfe3a2a9c66d3c54e308174a3
RasmusLeddyr <rasmusleddyr@hotmail.com>
2025-03-12 09:47:50 +0100

    Pages

---
+++ assets/js/html-insert.js
@@ -0,0 +1,8 @@
+fetch("/nav.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("nav-bar").innerHTML = data;
+  })
+  .catch((error) => {
+    console.error("Error loading nav file:", error);
+  });
