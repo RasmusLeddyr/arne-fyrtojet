fb3ba44bf012a12334c5634fd319709455968694
RasmusLeddyr <rasmusleddyr@gmail.com>
2025-03-19 11:27:24 +0100

    Personale style

--- assets/js/html-insert.js
+++
@@ -1,8 +0,0 @@
-fetch("../assets/insert-html/nav.html")
-  .then((response) => response.text())
-  .then((data) => {
-    document.getElementById("nav-bar").innerHTML = data;
-  })
-  .catch((error) => {
-    console.error("Error loading nav file:", error);
-  });
